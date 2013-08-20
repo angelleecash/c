76c016800e1aa4ba055572ea0d795ff888c33a56
Chen Liang <chenliangcareer@gmail.com>
2013-08-20 16:54:32 +0800

    debug output in vs2010

--- util.h
+++ util.h
@@ -8,7 +8,21 @@
 #define VERBOSE_HEADER ""
 
 #define STACK_DUMP(L) (VERBOSE ? (stack_dump(L)) : (EMPTY))
+
+#ifdef WIN32
+
+#include "windows.h"
+char _utilBuffer[1024];
+
+#define P(format, ...) \
+	{\
+		sprintf(_utilBuffer,format,__VA_ARGS__);\
+		OutputDebugString((_utilBuffer));\
+	}
+#else
 #define P(format, ...) (VERBOSE ? fprintf(stderr, VERBOSE_HEADER format , ## __VA_ARGS__) : (EMPTY))
+#endif
+
 
 
 void dump(char* p, int n);
